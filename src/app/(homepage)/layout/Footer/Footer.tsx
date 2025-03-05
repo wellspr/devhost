5457e17228bbff5eb58c852d03bde8443f9a9e80
Paulo Wells <paulowells@gmail.com>
2025-03-05 18:41:14 -0300

    added view on github button

--- src/app/(homepage)/layout/Footer/Footer.tsx
+++ src/app/(homepage)/layout/Footer/Footer.tsx
@@ -1,10 +1,21 @@
+import Link from "next/link";
 import "./footer.scss";
+import { IconBrandGithub } from "@tabler/icons-react";
 
 export const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer__content">
-                <div className="footer__content__copy">&copy; 2025 Dev Host</div>
+                <div className="footer__content__links">
+                    <Link className="footer__content__links__link-github"
+                        href={"https://github.com/wellspr/devhost"}>
+                        <span>View on Github</span>
+                        <IconBrandGithub />
+                    </Link>
+                </div>
+                <div className="footer__content__copy">
+                    &copy; 2025 Dev Host - All rights reserved
+                </div>
             </div>
         </footer>
     );
